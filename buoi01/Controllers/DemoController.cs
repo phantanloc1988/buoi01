cc173723b61041414175eb3b55e5f7d473cb3b16
NN <NN@phantanloc19898>
2020-05-10 11:49:23 +0700

    phantanloc

---
+++ buoi01/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace buoi01.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Chuoi()
+        {
+            
+            string a = string.Empty;
+            return "hello";
+        }
+
+        public int So()
+        {
+            return new Random().Next(1000, 10000);
+        }
+
+        public IActionResult TuDien()
+        {
+            return View();
+        }
+    }
+}
